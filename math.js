f5440ea6ce0c0b9efdb207cde8d8f9e325de06bb
Aditi Pandey <aditimech01@gmail.com>
2022-10-18 10:18:33 -0400

    Calculate tip amount

---
+++ math.js
@@ -0,0 +1,11 @@
+//buy 3 tacos @6.00 each; apply a coupon that gives me $3 off
+
+let tacoPrice = 6.00;
+console.log(typeof tacoPrice);
+
+let coupon = 3.00;
+
+let tacoQuantity = 3;
+let totalTacoPrice = tacoQuantity * tacoPrice;
+console.log(totalTacoPrice);
+
---
+++ tip_amount.js
@@ -0,0 +1,7 @@
+let foodBill = 50.00;
+
+let tip_percentage = 0.10;
+
+let tip_amount = foodBill * tip_percentage;
+console.log(tip_amount);
+
