a438da99ea35ad39703e3ffdce9e08753091c4d4
Aditi Pandey <aditimech01@gmail.com>
2022-10-19 18:24:53 -0400

    add conversion_tests

---
+++ conversion_tests.js
@@ -0,0 +1,79 @@
+//Description : This script tets various numeric conversion techniques
+//Author : Aditi Pandey
+
+
+// Define the Variable
+let a = "   101.1   ";
+let b = "55";
+let c = "402 Stevens";
+let d = "Number 5  ";
+
+//parseInt() Results
+a = parseInt(a);
+console.log(typeof a);
+console.log(a);
+
+b = parseInt(b);
+console.log(typeof b);
+console.log(b);
+
+
+c = parseInt(c);
+console.log(typeof c);
+console.log(c);
+
+d = parseInt(d);
+console.log(typeof d);
+console.log(d);
+
+
+//parseFloat() Results
+
+a = parseFloat(a);
+console.log(typeof a);
+console.log(a);
+
+b = parseFloat(b);
+console.log(typeof b);
+console.log(b);
+
+c = parseFloat(c);
+console.log(typeof c);
+console.log(c);
+
+d = parseFloat(d);
+console.log(typeof d);
+console.log(c);
+
+
+
+//Number()
+a = Number(a);
+console.log(typeof a);
+console.log(a);
+
+
+b = Number(b);
+console.log(typeof b);
+console.log(b);
+
+
+c = Number(c);
+console.log(typeof c);
+console.log(c);
+
+
+d = Number(d);
+console.log(typeof d);
+console.log(d);
+
+// unary +
+
+
+
+
+
+
+
+
+
